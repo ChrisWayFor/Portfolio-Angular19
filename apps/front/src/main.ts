44d55bd70c271b7a660cbb3fb2ea69331711506d
ChrisWayFor <christopher.farcoz@gmail.com>
2024-11-19 23:51:23 +0100

    feat/DAT-main fix and update @import to @use for tailwind

--- apps/front/src/main.ts
+++ apps/front/src/main.ts
@@ -3,7 +3,7 @@ import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { provideHttpClient, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -15,8 +15,8 @@ export function HttpLoaderFactory(http: HttpClient) {
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
+    provideHttpClient(),
     importProvidersFrom(
-      HttpClientModule,
       BrowserAnimationsModule,
       TranslateModule.forRoot({
         loader: {
